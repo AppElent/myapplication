ed377f1182c55cd2d12aa8e304ced0f6cbd27934
Eric Jansen <ericjansen@live.nl>
2019-02-09 22:18:55 +0100

    eerste commit

---
+++ backend/app/config/env.js
@@ -0,0 +1,16 @@
+
+const env = {
+  database: 'testdb',
+  username: 'root',
+  password: '12345',
+  host: 'localhost',
+  dialect: 'sqlite',
+  pool: {
+	  max: 5,
+	  min: 0,
+	  acquire: 30000,
+	  idle: 10000
+  }
+};
+ 
+module.exports = env;
