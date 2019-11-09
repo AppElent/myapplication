ec220a68f171c12ba3b7c1c54a1dc76309d96b4c
Eric Jansen <ericjansen@live.nl>
2019-11-09 14:09:39 +0100

    kostenoverzicht uitgebreid

--- client/src/views/Meterstanden/components/KostenOverzicht.js
+++ client/src/views/Meterstanden/components/KostenOverzicht.js
@@ -18,6 +18,10 @@ import {addData, updateData, deleteData} from 'modules/MaterialTable';
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(2)
+  },
+  textfields: {
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(1)
   }
 }));
 
@@ -26,13 +30,19 @@ const KostenOverzicht = ({}) => {
   const {user, userInfo, ref} = useSession();
   const {data, loading, error, ref: collectionRef} = useFirestoreCollectionData(ref.collection('energiekosten'));
 
-  const {hasError, isDirty, state, handleOnChange, handleOnSubmit, submitting, setInitial} = useForm({dal: 1000, normaal: 1000}, {}, () => {});
+  const {hasError, isDirty, state, handleOnChange, handleOnSubmit, submitting, setInitial} = useForm({dal: 1000, normaal: 1000, gas: 0, netbeheer: 0, verlaging_energiebelasting: 0}, {}, () => {}, {localStorage: 'energiekosten'});
 
   const columns = [{
     title: 'Leverancier',
     field: 'leverancier',
     required: true,
     editable: 'onAdd'
+  },{
+    title: 'Aanbieder',
+    field: 'aanbieder'
+  },{
+    title: 'Looptijd',
+    field: 'looptijd'
   },{
     title: 'Vaste kosten per jaar',
     field: 'vaste_kosten_jaar',
@@ -46,13 +56,38 @@ const KostenOverzicht = ({}) => {
     title: 'Prijs per KwH normaaltarief',
     field: 'kwh_prijs_normaal'
   },{
+    title: 'Prijs per M3 gas',
+    field: 'gas_prijs'
+  },{
     title: 'Korting per jaar',
     field: 'korting',
     required: true,
     initialEditValue: 0
   },{
     title: 'Totaal',
-    render: rowData => rowData ? '€' + (Math.round((rowData.vaste_kosten_jaar + (Number(rowData.kwh_prijs_dal) * state.dal.value) + (Number(rowData.kwh_prijs_normaal) * state.normaal.value) - rowData.korting) * 100) / 100): '€0' ,
+    render: rowData => rowData ? '€' + (Math.round(
+      (
+        (rowData.vaste_kosten_jaar || 0) + 
+        (Number(rowData.gas_prijs) * state.gas.value || 0) + 
+        (Number(rowData.kwh_prijs_dal) * state.dal.value || 0) + 
+        (Number(rowData.kwh_prijs_normaal) * state.normaal.value || 0) - 
+        (rowData.korting || 0) - 
+        (state.verlaging_energiebelasting.value || 0) + 
+        (state.netbeheer.value || 0)
+      ) * 100) / 100): '€0' ,
+    editable: 'never'
+  },{
+    title: 'Per maand',
+    render: rowData => rowData ? '€' + (Math.round(
+      (
+        (rowData.vaste_kosten_jaar || 0) + 
+        (Number(rowData.gas_prijs) * state.gas.value || 0) + 
+        (Number(rowData.kwh_prijs_dal) * state.dal.value || 0) + 
+        (Number(rowData.kwh_prijs_normaal) * state.normaal.value || 0) - 
+        (rowData.korting || 0) - 
+        (state.verlaging_energiebelasting.value || 0) + 
+        (state.netbeheer.value || 0)
+      ) * 100) / 100 / 12): '€0' ,
     editable: 'never'
   }]
 
@@ -60,20 +95,44 @@ const KostenOverzicht = ({}) => {
     <div className={classes.root}>
       <div className={classes.row}>
         <TextField
-          label="Verbruik dal"
+          className={classes.textfields}
+          label="Verbruik KwH dal"
           name="dal"
           onChange={handleOnChange}
           type="number"
-          value={state.dal.value}
-          variant="outlined"
+          value={state.dal.value || 0}
         />
         <TextField
-          label="Verbruik normaal"
+          className={classes.textfields}
+          label="Verbruik KwH normaal"
           name="normaal"
           onChange={handleOnChange}
           type="number"
-          value={state.normaal.value}
-          variant="outlined"
+          value={state.normaal.value || 0}
+        />
+        <TextField
+          className={classes.textfields}
+          label="Verbruik gas"
+          name="gas"
+          onChange={handleOnChange}
+          type="number"
+          value={state.gas.value || 0}
+        />
+        <TextField
+          className={classes.textfields}
+          label="Netbeheer kosten"
+          name="netbeheer"
+          onChange={handleOnChange}
+          type="number"
+          value={state.netbeheer.value || 0}
+        />
+        <TextField
+          className={classes.textfields}
+          label="Verlaging energiebelasting"
+          name="verlaging_energiebelasting"
+          onChange={handleOnChange}
+          type="number"
+          value={state.verlaging_energiebelasting.value || 0}
         />
       </div>
       <Table 
